458d2837e5a7a2198db38be38f794b644e0da980
kiko0o <kirilalamurov@gmail.com>
2022-12-23 18:00:13 +0200

    grid

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const cdiv = document.querySelector('.container');
 
-// Add 16 divs
 for (let i = 1; i < 257; i++) {
-    const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    cdiv.setAttribute('style', `grid-template-columns: repeat(16, 2fr); grid-template-rows: repeat(16, 2fr);`);
+    const div = document.createElement('div'); 
+    div.classList.add("cell");
     cdiv.appendChild(div);
+    div.addEventListener("mouseover", changeColor);
 }
+
+function changeColor() {
+    currentColor = window.getComputedStyle(this).backgroundColor;
+    this.style.backgroundColor = "blue";
+    console.log(this);
+
+}
+
+function newGrid() {
+    while (cdiv.firstChild) {
+        cdiv.removeChild(cdiv.lastChild);
+    }
+    let choice = prompt("Enter grid number", "16");
+    if (choice > 100 | isNaN(choice)) {
+        alert("Number lower than 100 required!")
+        location.reload();
+    }
+    for (let i = 1; i < (choice**2) + 1; i++) {
+        cdiv.setAttribute('style', `grid-template-columns: repeat(${choice}, 2fr); grid-template-rows: repeat(${choice}, 2fr);`);
+        const div = document.createElement('div');
+        div.classList.add("cell");
+        cdiv.appendChild(div);
+        div.addEventListener("mouseover", changeColor);
+    }
+}
+
+
